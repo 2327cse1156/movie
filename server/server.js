e9b3a2f06a8357dd418f03c619e8fa46201e0784
2327cse1156 <anshkaushal92@gmail.com>
2025-08-12 18:34:40 +0530

    inngest setup completed and connected to clerk

--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/db.js";
-import {clerkMiddleware} from "@clerk/express"
+import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+import { functions, inngest } from "./inngest/index.js";
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -10,13 +12,20 @@ await connectDB();
 // middleware
 app.use(express.json());
 app.use(cors());
-app.use(clerkMiddleware())
+app.use(clerkMiddleware());
+app.use(
+  "/api/inngest",
+  serve({
+    client: inngest,
+    functions,
+  })
+);
 
 // api routes
-app.get("/", (req,res)=>{
-    res.send("Server is live");
-})
+app.get("/", (req, res) => {
+  res.send("Server is live");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
