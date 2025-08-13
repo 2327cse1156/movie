3a9a11f89582d0fb691e09e9234365a2aa5d747e
2327cse1156 <anshkaushal92@gmail.com>
2025-08-13 20:28:21 +0530

    admin controller updated

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ import { serve } from "inngest/express";
 import { functions, inngest } from "./inngest/index.js";
 import showRouter from "./routes/showRoutes.js"
 import bookingRouter from "./routes/bookingRoutes.js";
+import adminRouter from "./routes/adminRoutes.js";
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -24,6 +25,7 @@ app.use(
 );
 app.use("/api/show",showRouter)
 app.use("/api/booking",bookingRouter)
+app.use("/api/admin", adminRouter)
 // api routes
 app.get("/", (req, res) => {
   res.send("Server is live");
