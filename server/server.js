aa59f78499092890d9ebf037c1fb540b6e23e830
2327cse1156 <anshkaushal92@gmail.com>
2025-08-20 17:55:44 +0530

    complete backend

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ import { functions, inngest } from "./inngest/index.js";
 import showRouter from "./routes/showRoutes.js"
 import bookingRouter from "./routes/bookingRoutes.js";
 import adminRouter from "./routes/adminRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -26,6 +27,7 @@ app.use(
 app.use("/api/show",showRouter)
 app.use("/api/booking",bookingRouter)
 app.use("/api/admin", adminRouter)
+app.use("/api/user",userRouter)
 // api routes
 app.get("/", (req, res) => {
   res.send("Server is live");
